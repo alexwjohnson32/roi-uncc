452299a3a4504b256db02e7ab8e9b002ad517c7c
Ehab Shoubaki <eshoubak@charlotte.edu>
2025-02-23 22:19:15 -0500

    - Reimplemented the LC tank example to use the HELICS C++ API.

--- examples/lc-tank/cpp/Capacitor.cpp
+++ examples/lc-tank/cpp/Capacitor.cpp
@@ -22,64 +22,65 @@ int main() {
   // Set some flags
   fi.setFlagOption(HELICS_FLAG_UNINTERRUPTIBLE, false);
   fi.setFlagOption(HELICS_FLAG_TERMINATE_ON_ERROR, true);
-  fi.setFlagOption(HELICS_FLAG_WAIT_FOR_CURRENT_TIME_UPDATE,false);
+  fi.setFlagOption(HELICS_FLAG_WAIT_FOR_CURRENT_TIME_UPDATE,true);
 
   // Create Value Federate
-  helics::ValueFederate Inductor("Inductor",fi);
-  std::cout << "HELICS Inductor Federate created successfully." << std::endl;
+  helics::ValueFederate Capacitor("Capacitor",fi);
+  std::cout << "HELICS Capacitor Federate created successfully." << std::endl;
 
   // Registering Publications and subscriptions
-  auto Il_id = Inductor.registerPublication("Il", "double", "A");
-  auto Vc_id = Inductor.registerSubscription("Capacitor/Vc", "V");
+  auto Vc_id = Capacitor.registerPublication("Vc", "double", "V");
+  auto Il_id = Capacitor.registerSubscription("Inductor/Il", "A");
 
   // Enter execution mode (this transitions the federate from initialization to execution)
-  Inductor.enterExecutingMode();
+  Capacitor.enterExecutingMode();
   std::cout << "Federate has entered execution mode." << std::endl;
+
   // Simulation Initialization
   double total_interval = 10.0;
   double grantedtime = 0.0;
-  double l_value = 0.159;
-  double capacitor_voltage = 0.0;
-  double delta_i = 0.0;
+  double c_value = 0.159;
+  double inductor_current = 0.0;
+  double delta_v = 0.0;
 
   // Data collection vectors
   std::vector<double> time_sim;
-  std::vector<double> current;
+  std::vector<double> voltage;
 
   time_sim.push_back(grantedtime);
-  current.push_back(1.0);
+  voltage.push_back(0.0);
 
-  // Publish Initial current
-  Il_id.publish(current.back());
+  // Publish Initial voltage
+  Vc_id.publish(voltage.back());
 
   // Entering simulation loop
   while (grantedtime < total_interval) {
     // Request time
-    grantedtime = Inductor.requestTime(grantedtime+period);
+    grantedtime = Capacitor.requestTime(grantedtime+period);
 
-    // Get Capacitor Voltage charging the inductor, calculate delta_i
-    capacitor_voltage = Vc_id.getDouble();
-    delta_i = (1.0 / l_value) * capacitor_voltage * period;
+    // Get Inductor current, calculate delta_v
+    inductor_current = Il_id.getDouble();
+    delta_v = (-1.0 / c_value) * inductor_current * period;
 
     // Store updates
     time_sim.push_back(grantedtime);
-    current.push_back(current.back() + delta_i);
+    voltage.push_back(voltage.back() + delta_v);
 
-    // Publish new inductor current
-    Il_id.publish(current.back());
+    // Publish new Capacitor voltage
+    Vc_id.publish(voltage.back());
   }
 
   // Write results into CSV file
-  std::ofstream outFile("Inductor_Current.csv");
-  outFile << "Time (s), Inductor Current (A)\n";
+  std::ofstream outFile("Capacitor_Voltage.csv");
+  outFile << "Time (s), Capacitor Voltage (V)\n";
   for (size_t i = 0; i < time_sim.size(); ++i) {
-    outFile << time_sim[i] << ", " << current[i] << "\n";
+    outFile << time_sim[i] << ", " << voltage[i] << "\n";
   }
   outFile.close();
 
   
   // Finalize the federate to clean up and disconnect from the HELICS core
-  Inductor.finalize();
+  Capacitor.finalize();
   std::cout << "Federate finalized." << std::endl;
 
   return 0;
