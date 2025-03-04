a634e465b70dc04c217d1c0cc8cdc30ccefb2e2d
Ehab Shoubaki <eshoubak@charlotte.edu>
2025-03-04 16:51:21 -0500

    - Started on the 2bus-IEEE13bus cosimulation, still has issues.

---
+++ examples/2bus-13bus/build/CMakeFiles/gpk-left-fed.x.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/ehab/roi-uncc/2bus-13bus/gpk-left-fed.cpp" "CMakeFiles/gpk-left-fed.x.dir/gpk-left-fed.cpp.o" "gcc" "CMakeFiles/gpk-left-fed.x.dir/gpk-left-fed.cpp.o.d"
+  "/home/ehab/roi-uncc/2bus-13bus/pf_app.cpp" "CMakeFiles/gpk-left-fed.x.dir/pf_app.cpp.o" "gcc" "CMakeFiles/gpk-left-fed.x.dir/pf_app.cpp.o.d"
+  "/home/ehab/roi-uncc/2bus-13bus/pf_factory.cpp" "CMakeFiles/gpk-left-fed.x.dir/pf_factory.cpp.o" "gcc" "CMakeFiles/gpk-left-fed.x.dir/pf_factory.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
